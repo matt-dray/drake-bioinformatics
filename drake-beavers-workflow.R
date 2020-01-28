4a6101b34147fe3086d48948455ae268b597cfbb
Matt Dray <mwdray@gmail.com>
2020-01-28 22:44:08 +0000

    Provide the code in a single script

--- drake-beavers-workflow.R
+++ drake-beavers-workflow.R
@@ -1,14 +1,26 @@
+# Reproducible workflows with {drake}
+# Bioinformatics London Meetup, 2020-01-30
+# This is a script file containing the code from the talk slides
+# Slides available here: https://matt-dray.github.io/drake-bioinformatics/
+
+# Packages ----------------------------------------------------------------
+
 library(drake)
 library(dplyr)
 library(ggplot2)
 library(rphylopic)
 
+# Functions ---------------------------------------------------------------
+
+# Simple beaver plot
 b_plot <- function(data, image) {
   ggplot(data, aes(id, temp)) +
     geom_boxplot() +
     labs(title = "Beaver temperature") +
     add_phylopic(image)
 }
+
+# Simple beaver summary table
 b_table <- function(data) {
   beavers_trim <- data %>% 
     group_by(id) %>% 
@@ -19,6 +31,9 @@ b_table <- function(data) {
   return(beavers_trim)
 }
 
+# Plan --------------------------------------------------------------------
+
+# Wrap analysis steps in drake_plan()
 plan <- drake_plan(
   # wrangle data
   b1 = mutate(beaver1, id = "A"),
@@ -31,9 +46,31 @@ plan <- drake_plan(
   plot = b_plot(beavers, png),
   table = b_table(beavers),
   report = rmarkdown::render(
-    knitr_in("beavers-report.Rmd"), quiet = TRUE,
-    output_file = file_out("beaver-report.html")
+    knitr_in("beavers-report.Rmd"), quiet = TRUE,  # note knitr_in()
+    output_file = file_out("beaver-report.html")  # note file_out()
   )
 )
 
-drake::make(plan)
+# Make --------------------------------------------------------------------
+
+drake::make(plan)  # executes the analysis steps in the plan
+
+# Inspection --------------------------------------------------------------
+
+# Get cached objects
+cached()  # check what's in the cache
+readd()  # return an object from the cache
+
+# Create network graph
+config <- drake_config(plan)  # make a configuration file for the plan
+vis_drake_graph(config)  # build an interactive network graph using the config
+
+# Make changes ------------------------------------------------------------
+
+# Let's say something in your workflow changed. What is now out of date?
+
+outdated()  # prints the targets that are out of date
+
+vis_drake_graph(config)  # rebuild grpah to see impacted targets coloured black
+
+drake::make(plan)  # re-make the plan!
